01f628159b844cbfcff7f92b9dc7a7c882eeef79
syahjamal <sulaimansyahjamal@gmail.com>
2021-02-04 11:00:43 +0700

    Creating the sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Bars, Nav, NavIcon, NavLink } from './NavbarElements';
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
     return (
         <>
             <Nav>
                 <NavLink to='/'>
                     Pizza
                 </NavLink>
-                <NavIcon>
+                <NavIcon onClick={toggle}>
                     <p>Menu</p>
                     <Bars/>
                 </NavIcon>
