27344fe81e1fd6b904e64f9e9533d9a94e708913
syahjamal <sulaimansyahjamal@gmail.com>
2021-02-06 12:30:18 +0700

    creating featured components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
 import Products from './components/Products';
-import {productData} from './components/Products/data';
+import {productData, productDataTwo} from './components/Products/data';
+import Feature from './components/Feature';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <GlobalStyle/>
       <Hero />
       <Products heading='Choose your favorite' data={productData}/>
+      <Feature/>
+      <Products heading='Sweet Treats for You' data={productDataTwo}/>
     </Router>
   );
 }
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,11 @@
-import product1 from '../../images/pizza-2.png';
+import product1 from '../../images/pizza-1.png';
 import product2 from '../../images/pizza-2.png';
-import product3 from '../../images/pizza-2.png';
+import product3 from '../../images/pizza-4.png';
+import sweet1 from '../../images/dessert-1.jpg';
+import sweet2 from '../../images/dessert-2.jpg';
+import sweet3 from '../../images/dessert-3.jpg';
+
+
 
 export const productData = [
     {
@@ -24,6 +29,33 @@ export const productData = [
         alt: 'Pizza',
         name: 'Veggie',
         desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+]
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt: 'Cake',
+        name: 'Cream Cake Delicious',
+        desc: 'Cryspy cake, whip cream, starwberry',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet2,
+        alt: 'Chocolate',
+        name: 'Volcano Cake',
+        desc: 'Chocolate cake, choco sauce, beery, sweet sugar',
+        price: '$19.99',
+        button: 'Add to Cart'
+    },
+    {
+        img: sweet3,
+        alt: 'Pancake',
+        name: 'Pancake Bee',
+        desc: 'Pancake puzzle, strawberry, honey',
         price: '$19.99',
         button: 'Add to Cart'
     },
